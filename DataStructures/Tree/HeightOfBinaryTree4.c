e2e58bfc9c77022eee649c2408cabb0aed08d9e9
Alok Sharma <aloks1701@gmail.com>
2016-04-16 02:20:02 +0530

    Create HeightOfBinaryTree4.c

---
+++ DataStructures/Tree/HeightOfBinaryTree4.c
@@ -0,0 +1,16 @@
+
+//Incomplete
+/*The tree node has data, left child and right child 
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+int height(node * root)
+{
+  
+}
+  

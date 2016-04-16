2d6f892c0a7d9c728625d68367993da76195e2d9
Alok Sharma <aloks1701@gmail.com>
2016-04-16 10:17:18 +0530

    Update DiagonalDifferece.c

--- Algorithms/Warmup/DiagonalDifferece.c
+++ Algorithms/Warmup/DiagonalDifferece.c
@@ -16,5 +16,20 @@ int main(){
           scanf("%d",&a[a_i][a_j]);
        }
     }
+    int diag1=0, diag2=0;
+    for( int i=0,j=0; i<n, j<n; i++, j++) {
+        diag1 += a[i][j]; //For main diagonal, ith and jth both index increase by 1 in each iteration
+    }
+    for(int i=0, j =n-1; i<n, j>=0; i++, j--) 
+        {                       //For second diagonal, ith index increases and jth index decreases in same interation
+        diag2 += a[i][j]; 
+        //printf("%d\n",diag2);    
+    }
+    int result = abs(diag1 - diag2);
+   /* printf("%d\n",diag1);
+    printf("%d\n",diag2); */ //used for debugging 
+    printf("%d",result);
+        
+    
     return 0;
 }

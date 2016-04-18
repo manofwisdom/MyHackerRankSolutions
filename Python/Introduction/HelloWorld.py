6a7cd74f3b6c54ff992323243632a04149ff2f63
Alok Sharma <aloks1701@gmail.com>
2016-04-19 00:45:36 +0530

    Create HelloWorld.py

---
+++ Python/Introduction/HelloWorld.py
@@ -0,0 +1,7 @@
+#write your code in next line. 
+#This is a comment in python
+#We simply use print variable or print string in python to print something on the screen in python 2.x
+#In python 3.x we use the function print() with variable name or string as a parameter
+
+my_string = "Hello World!"
+print my_string

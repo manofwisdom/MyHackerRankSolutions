3b727c5d9232f56e6f3cc0768956f44dd62508ee
Alok Sharma <aloks1701@gmail.com>
2016-04-28 17:38:49 +0530

    Create PrintFunction.py

---
+++ Python/Introduction/PrintFunction.py
@@ -0,0 +1,3 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function #Needed in Python2.x, modifies the print function
+for i in range(1, int(raw_input())+1): print(i, sep='', end='')

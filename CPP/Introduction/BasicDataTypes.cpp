b667548866975d187cc5f67abd314fed93e77ce3
Alok Sharma <aloks1701@gmail.com>
2016-05-11 17:12:09 +0530

    Update BasicDataTypes.cpp

--- CPP/Introduction/BasicDataTypes.cpp
+++ CPP/Introduction/BasicDataTypes.cpp
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include<stdio.h>
 
+
+int main() {
+  
+     // Complete the code.
+    int n;
+    long l;
+    long long ll;
+    char a;
+    float f;
+    double d;
+    
+    scanf("%d %ld %lld %c %f %lf",&n,&l,&ll,&a,&f,&d);
+    printf("%d\n%ld\n%lld\n%c\n%f\n%lf",n,l,ll,a,f,d);
+
+    return 0;
+}
